445528edaf303dc2a2a1330fe6654a258c3ca302
George0717 <raden7george@gmail.com>
2025-01-07 01:37:57 +0700

    sales order dan jadwal kirim aman

--- app/Models/JadwalKirim.php
+++ app/Models/JadwalKirim.php
@@ -5,11 +5,10 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Spatie\Activitylog\LogOptions;
-use Spatie\Activitylog\Traits\LogsActivity;
 
 class JadwalKirim extends Model
 {
-    use HasFactory, LogsActivity;
+    use HasFactory;
 
     protected $fillable = [
         'sales_order_id',
@@ -23,10 +22,4 @@ class JadwalKirim extends Model
         return $this->belongsTo(SalesOrder::class);
     }
 
-    public function getActivitylogOptions(): LogOptions
-    {
-        return LogOptions::defaults()
-            ->setDescriptionForEvent(fn(string $eventName) => "Jadwal Kirim telah {$eventName}")
-            ->logOnly(['sales_order_id', 'delivery_date', 'keterangan', 'tujuan_pengiriman']);
-    }
 }
--- resources/views/pages/SalesOrder/create.blade.php
+++ resources/views/pages/SalesOrder/create.blade.php
@@ -42,11 +42,6 @@
                 @enderror
             </div>
 
-            <div class="mb-4">
-                <label for="so_number" class="block text-sm font-medium text-gray-700">Nomor SO</label>
-                <input type="text" id="so_number" name="so_number" class="mt-1 block w-full border-gray-300 rounded-md shadow-sm focus:border-indigo-500 focus:ring focus:ring-indigo-500 focus:ring-opacity-50" placeholder="No.Surat/Nama Perusahaan/Bulan/Tahun">
-            </div>
-
              <!-- File Upload -->
              <div class="mb-4">
                 <label for="po_photo" class="block text-sm font-medium text-gray-700">Photo PO</label>
